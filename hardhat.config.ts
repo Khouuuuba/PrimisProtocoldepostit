6790a93e6bcf99d1f4e7d669d44dfc8810422f58
Khouuba <161449971+Khouuuuba@users.noreply.github.com>
2024-02-28 18:37:35 +0100

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,7 +6,7 @@ require("dotenv").config();
 // dotenv.config();
 
 module.exports = {
-  // defaultNetwork: "",
+  // defaultNetwork: "matic",
   networks: {
     ethereum: {
       // url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_PVT_KEY}`,
