ce4cee4e36269c953beaa231ce64d225a250d1fe
Khouuba <161449971+Khouuuuba@users.noreply.github.com>
2024-02-28 23:47:53 +0100

    Update and rename ISEndToken.sol to ISPrmToken.sol

--- contracts/interfaces/ISPrmToken.sol
+++ contracts/interfaces/ISPrmToken.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.8.18;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /**
- * @title IEndToken interface
- * @notice Interface for the EndToken contract
+ * @title IPrmToken interface
+ * @notice Interface for the PrmToken contract
  */
-interface ISEndToken is IERC20 {
+interface ISPrmToken is IERC20 {
     function mint(address to, uint256 amount) external;
 
     function burn(address from, uint256 amount) external;
