2afbfe942fbb699d6181aac2cd6e6d3a8a6b15cf
Khouuba <161449971+Khouuuuba@users.noreply.github.com>
2024-02-28 23:45:33 +0100

    Update ILido.sol

--- contracts/interfaces/ILido.sol
+++ contracts/interfaces/ILido.sol
@@ -4,5 +4,5 @@ pragma solidity ^0.8.18;
 interface ILido{
     function submit(address _referral) external payable returns (uint256);
 
-    // function withdrawStEth(EndRequest memory) external returns (uint256);
+    // function withdrawStEth(PrmRequest memory) external returns (uint256);
 }
